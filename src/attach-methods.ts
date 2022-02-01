c2c87563b0d5076d2798087dfde22e48af835d74
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-02-01 20:11:55 +0300

    perhaps not needed

--- src/attach-methods.ts
+++ src/attach-methods.ts
@@ -5,43 +5,66 @@ import { attachOutput } from './attach-output';
 import { attachSelect } from './attach-select';
 import { attachTextarea } from './attach-textarea';
 import { FieldMethods, Unsubscribe } from './i-face-methods';
-const enum HonoredTags {
-  BUTTON = 'BUTTON',
-  FIELDSET = 'FIELDSET',
-  INPUT = 'INPUT',
-  OUTPUT = 'OUTPUT',
-  SELECT = 'SELECT',
-  TEXTAREA = 'TEXTAREA',
+
+const honoredKeys = [
+  'button',
+  'fieldset',
+  'input',
+  'output',
+  'select',
+  'textarea',
+] as const;
+
+type HonoredElements = Pick<HTMLElementTagNameMap, typeof honoredKeys[number]>
+
+type HonoredGuards = {
+  [k in keyof HonoredElements]: (element: Element) => element is HTMLElementTagNameMap[Lowercase<k & string>]
 }
 
-const guards = {
-  button:(element:Element):element is HTMLButtonElement => element.tagName ===HonoredTags.BUTTON,
-  fieldset:(element:Element):element is HTMLFieldSetElement => element.tagName ===HonoredTags.FIELDSET,
-  input:(element:Element):element is HTMLInputElement => element.tagName ===HonoredTags.INPUT,
-  output:(element:Element):element is HTMLOutputElement => element.tagName ===HonoredTags.OUTPUT,
-  select:(element:Element):element is HTMLSelectElement => element.tagName ===HonoredTags.SELECT,
-  textarea: (element:Element):element is HTMLTextAreaElement => element.tagName ===HonoredTags.TEXTAREA,
+type HonoredAttach = {
+  [k in keyof HonoredElements]: (element: HonoredElements[k], methods: FieldMethods) => void;
+}
+
+const guards: HonoredGuards = honoredKeys.reduce((a, b) => {
+  a[b] = (element): element is never => element.tagName.toLowerCase() === b;
+  return a;
+}, {} as HonoredGuards);
+
+const attach: HonoredAttach = {
+  button: attachButton,
+  fieldset: attachFieldset,
+  input: attachInput,
+  output: attachOutput,
+  select: attachSelect,
+  textarea: attachTextarea
 };
 
+export const attachMethods = (element: Element, methods: FieldMethods): Unsubscribe[] => {
 
-export const attachMethods = (element:Element, methods:FieldMethods):Unsubscribe[]=>{
 
-  if(guards.button(element)){
+  honoredKeys.some((k) => {
+    if (guards[k](element)) {
+      attach[k](element as never, methods);
+    }
+  });
+
+  if (guards.button(element)) {
+    attach.button(element, methods);
     return attachButton(element, methods);
   }
-  if(guards.fieldset(element)){
+  if (guards.fieldset(element)) {
     return attachFieldset(element, methods);
   }
-  if(guards.input(element)){
+  if (guards.input(element)) {
     return attachInput(element, methods);
   }
-  if(guards.output(element)){
+  if (guards.output(element)) {
     return attachOutput(element, methods);
   }
-  if(guards.select(element)){
+  if (guards.select(element)) {
     return attachSelect(element, methods);
   }
-  if(guards.textarea(element)){
+  if (guards.textarea(element)) {
     return attachTextarea(element, methods);
   }
   return [];
